4ddac2cd107ea6b35197242ca4bac03a19f0a4b7
Cutter72 <drelich_pawel@o2.pl>
2019-09-26 21:40:22 +0200

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {WarningAlertComponent} from './warning-alert/warning-alert.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ export class AppComponent {
   private newMessageAllow = false;
   private alertCreationStatus = 'No alert was created!';
   private alertName = 'TEST alert init name';
-  private resetAllow = true;
+  private isCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -22,7 +23,11 @@ export class AppComponent {
   }
 
   onAlertCreation() {
-    this.alertCreationStatus = 'Alert was created! Name is "' + this.alertName + '"';
+    this.isCreated = true;
+    this.alertCreationStatus = 'Last added alert was "' + this.alertName + '"';
+    setTimeout(() => {
+      this.isCreated = false;
+    }, 4000);
   }
 
   onUpdateAlertName(event: any) {
@@ -34,7 +39,7 @@ export class AppComponent {
   }
 
   isResetAllowed() {
-    if (this.alertName == '' || !this.isNewMessageAllowed()) {
+    if (this.alertName === '' || !this.isNewMessageAllowed()) {
       return false;
     }
     return true;
